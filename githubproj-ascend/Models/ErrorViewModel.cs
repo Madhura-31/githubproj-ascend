0f74f6e2a11e238236f18caa141dade4414130af
rohan <rohan@example.com>
2025-09-02 09:28:00 +0530

    Add project files.

---
+++ githubproj-ascend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace githubproj_ascend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
